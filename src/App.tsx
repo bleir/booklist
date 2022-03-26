b92bf3cec1a9bd92f2785a8f47a9aecddbe1069f
adam <adamsalej@gmail.com>
2022-03-26 16:17:36 +0100

    change commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import Button from '@mui/material/Button';
 import { BookList } from './components/BookList/BookList';
 import { Header } from './components/Header/Header';
 
+
 import './App.css';
 
+const handleClick = () => {
+  console.log('click!!!');
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
       <BookList />
-      Nowy commit
+      <Button variant="contained" onClick={handleClick}>Click me!</Button>
+      Nowy commit nower 
     </div>
   );
 }
